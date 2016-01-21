c8cb68aca4c4fcdaa4b5b313aecb3086df497628
innovatitw <murphy@innovati.com.tw>
2016-01-21 11:30:46 +0800

    Update ozone.js

--- ozone.js
+++ ozone.js
@@ -900,7 +900,7 @@
   var descriptor = {
     blocks: blocks[lang],
     menus: menus[lang],
-    url: 'http://innovatitw.github.io/innoScratch'
+    url: 'http://khanning.github.io/scratch-arduino-extension'
   };
 
   ScratchExtensions.register('Arduino', descriptor, ext, {type:'serial'});
